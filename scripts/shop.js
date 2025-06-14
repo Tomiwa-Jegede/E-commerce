6541a24b136e68f50b47ea286789521fa365fe36
Tomiwa Jegede <jegedetomiwa1@gmail.com>
2025-06-14 11:34:28 +0100

    i created the checkout page for the site

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,2 +1,16 @@
-import{dropdown} from "./index.js"
+import { dropdown } from "./index.js"
+import { products } from "./product.js"
 dropdown();
+
+const productContainer = document.querySelector('.product-container');
+let productHTML = '';
+products.forEach(p => {
+  productHTML += ` <div class="products">
+        <img src=${p.img} alt="">
+        Price:${p.price}
+        <button class="add-to-cart">
+          ADD TO CART
+        </button>
+      </div>`
+})
+productContainer.innerHTML = productHTML;
