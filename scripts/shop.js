65ee2cfc086b9f682a5684db600641c1962f6e50
Tomiwa Jegede <jegedetomiwa1@gmail.com>
2025-06-14 14:46:10 +0100

    you can now add to cart

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,16 +1,28 @@
 import { dropdown } from "./index.js"
 import { products } from "./product.js"
 dropdown();
-
 const productContainer = document.querySelector('.product-container');
 let productHTML = '';
 products.forEach(p => {
   productHTML += ` <div class="products">
         <img src=${p.img} alt="">
         Price:${p.price}
-        <button class="add-to-cart">
+        <button id=${p.id} class="add-to-cart">
           ADD TO CART
         </button>
       </div>`
 })
 productContainer.innerHTML = productHTML;
+/*---addToCart----*/
+const cart =JSON.parse(localStorage.getItem('cart')) || [];
+const addToCart = document.querySelectorAll('.add-to-cart');
+addToCart.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const productToAdd = products.find(product => product.id === btn.id);
+    const isInCart = cart.find(itemInCart => itemInCart.id === productToAdd.id);
+    if (isInCart) { console.log('items is already in the cart') }
+    else { cart.push(productToAdd) }
+    localStorage.setItem('cart', JSON.stringify(cart))
+    console.log(cart)
+  })
+})
